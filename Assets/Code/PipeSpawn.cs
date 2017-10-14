1e89080fc9949a7d8e5b7219169f49d6d016a779
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-10-14 00:07:27 -0300

    Pipes are now able to spawn on top and bottom simultaneously

--- Assets/Code/PipeSpawn.cs
+++ Assets/Code/PipeSpawn.cs
@@ -15,27 +15,33 @@ public class PipeSpawn : MonoBehaviour
     {
         GameObject newPipe = Instantiate(pipe);
         newPipe.transform.parent = this.transform;
+
         if (UnityEngine.Random.Range(0, 2) == 0)
         {
             newPipe.GetComponent<SpriteRenderer>().sprite = bottomPipe;
-            newPipe.transform.position = new Vector3(startPosX, RoundToNearestHalf(UnityEngine.Random.Range(-0.5f, -2f)), -5);
+            newPipe.transform.position = new Vector3(startPosX, UnityEngine.Random.Range(-0.5f, -2f), -5);
+
+            if (UnityEngine.Random.Range(0, 3) == 0 && newPipe.transform.position.y <= -0.5f)
+            {
+                GameObject secondaryNewPipe = Instantiate(pipe);
+                secondaryNewPipe.transform.parent = this.transform;
+                secondaryNewPipe.GetComponent<SpriteRenderer>().sprite = topPipe;
+                secondaryNewPipe.transform.position = new Vector3(startPosX, UnityEngine.Random.Range(2 + newPipe.transform.position.y, 2f), -5);
+            }
         }
         else
         {
             newPipe.GetComponent<SpriteRenderer>().sprite = topPipe;
-            newPipe.transform.position = new Vector3(startPosX, RoundToNearestHalf(UnityEngine.Random.Range(0.5f, 2f)), -5);
+            newPipe.transform.position = new Vector3(startPosX,UnityEngine.Random.Range(0.5f, 2f), -5);
+
+            if (UnityEngine.Random.Range(0, 3) == 0 && newPipe.transform.position.y >= -1f)
+            {
+                GameObject secondaryNewPipe = Instantiate(pipe);
+                secondaryNewPipe.transform.parent = this.transform;
+                secondaryNewPipe.GetComponent<SpriteRenderer>().sprite = bottomPipe;
+                secondaryNewPipe.transform.position = new Vector3(startPosX, UnityEngine.Random.Range(1 - newPipe.transform.position.y, -2f), -5);
+            }
         }
 
-
-    }
-
-    private void Update()
-    {
-
-    }
-
-    private float RoundToNearestHalf(float value)
-    {
-        return (float)Math.Round(value * 2) / 2;
     }
 }
