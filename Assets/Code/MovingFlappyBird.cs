d814344c031f76bb296127885eae532d49933008
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-10-14 19:00:44 -0300

    Bird now flaps

--- Assets/Code/MovingFlappyBird.cs
+++ Assets/Code/MovingFlappyBird.cs
@@ -6,11 +6,38 @@ public class MovingFlappyBird : MonoBehaviour
 {
     public float jump = 50;
 
-    void Update()  
+    public Sprite[] birdSprites;
+
+    private int spriteIndex = 0;
+
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0, jump, 0);
+            rigidBody.velocity = new Vector3(0, jump, 0);
+        }
+    }
+
+    void LateUpdate()
+    {
+        if(Time.frameCount % 10 == 0)
+        {
+            spriteIndex++;
+            if (spriteIndex >= birdSprites.Length)
+            {
+                spriteIndex = 0;
+            }
+
+            spriteRenderer.sprite = birdSprites[spriteIndex];
         }
     }
 }
