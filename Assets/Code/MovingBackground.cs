abdad05e2a3a3194cbc0144e3c1fe1103c7bf892
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-10-13 22:48:09 -0300

    Added scrolling background

---
+++ Assets/Code/MovingBackground.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBackground : MonoBehaviour
+{
+    public float movingSpeed = 4;
+    public float endXLocation, startXLocation;
+
+    void Update()
+    {
+        this.transform.position = new Vector2(this.transform.position.x - (movingSpeed * Time.deltaTime), this.transform.position.y);
+
+        if(this.transform.position.x < endXLocation)
+        {
+            this.transform.position = new Vector2(startXLocation, this.transform.position.y);
+        }
+    }
+}
